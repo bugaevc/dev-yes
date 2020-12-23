fbaa9758d67364919dfb754bff7026ca012a0eda
Sergey Bugaev <bugaevc@gmail.com>
2020-12-23 15:33:24 +0300

    Try to use locking

--- linux/yes.c
+++ linux/yes.c
@@ -3,8 +3,13 @@
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
+#include <linux/rwsem.h>
+#include <linux/mutex.h>
+
+DEFINE_MUTEX(private_data_mutex);
 
 struct yes_data {
+	struct rw_semaphore sem;
 	char *written_data;
 	size_t written_size;
 	char *exploded_data;
@@ -63,13 +68,35 @@ static long explode(struct yes_data *data)
 
 static ssize_t yes_read(struct file *file, char __user *buf, size_t count, loff_t *offset)
 {
-	struct yes_data *data = file->private_data;
+	struct yes_data *data = READ_ONCE(file->private_data);
 	size_t nread = 0;
+	long ret = 0;
 
-	if (unlikely(!data))
-		data = file->private_data = kmalloc(sizeof(struct yes_data), GFP_KERNEL | __GFP_ZERO);
-	if (unlikely(!data->exploded_data))
-		explode(data);
+	if (unlikely(!data)) {
+		if (mutex_lock_interruptible(&private_data_mutex))
+			return -ERESTARTSYS;
+		data = file->private_data;
+		if (likely(!data)) {
+			data = file->private_data = kmalloc(sizeof(struct yes_data), GFP_KERNEL | __GFP_ZERO);
+			init_rwsem(&data->sem);
+		}
+		mutex_unlock(&private_data_mutex);
+	}
+
+	if (down_read_killable(&data->sem))
+		return -ERESTARTSYS;
+	if (unlikely(!data->exploded_data)) {
+		up_read(&data->sem);
+		if (down_write_killable(&data->sem))
+			return -ERESTARTSYS;
+		if (likely(!data->exploded_data))
+			ret = explode(data);
+		if (unlikely(ret)) {
+			up_write(&data->sem);
+			return ret;
+		}
+		downgrade_write(&data->sem);
+	}
 
 	while (nread < count) {
 		size_t to_copy = min(count - nread, data->exploded_size - data->position);
@@ -77,31 +104,53 @@ static ssize_t yes_read(struct file *file, char __user *buf, size_t count, loff_
 		nread += copied;
 		data->position += copied;
 		data->position %= data->exploded_size;
-		if (copied < to_copy)
-			return -EFAULT;
+		if (copied < to_copy) {
+			ret = -EFAULT;
+			break;
+		}
 	}
 
+	up_read(&data->sem);
+	if (ret)
+		return ret;
 	return nread;
 }
 
 static ssize_t yes_write(struct file *file, const char __user *buf, size_t count, loff_t *offset)
 {
-	struct yes_data *data = file->private_data;
+	struct yes_data *data = READ_ONCE(file->private_data);
 
 	if (likely(!data)) {
+		if (mutex_lock_interruptible(&private_data_mutex))
+			return -ERESTARTSYS;
+		data = file->private_data;
+		if (unlikely(data)) {
+			mutex_unlock(&private_data_mutex);
+			goto do_realloc;
+		}
 		data = file->private_data = kmalloc(sizeof(struct yes_data), GFP_KERNEL | __GFP_ZERO);
 		data->written_data = memdup_user(buf, count);
 		data->written_size = count;
-	} else {
-		data->written_data = krealloc(data->written_data, data->written_size + count, GFP_USER);
-		if (copy_from_user(data->written_data + data->written_size, buf, count))
-			return -EFAULT;
-		data->written_size += count;
-		kfree(data->exploded_data);
-		data->exploded_data = NULL;
-		data->exploded_size = 0;
+		init_rwsem(&data->sem);
+		mutex_unlock(&private_data_mutex);
+		return count;
 	}
 
+do_realloc:
+	if (down_write_killable(&data->sem))
+		return -ERESTARTSYS;
+
+	data->written_data = krealloc(data->written_data, data->written_size + count, GFP_USER);
+	if (copy_from_user(data->written_data + data->written_size, buf, count)) {
+		up_write(&data->sem);
+		return -EFAULT;
+	}
+	data->written_size += count;
+	kfree(data->exploded_data);
+	data->exploded_data = NULL;
+	data->exploded_size = 0;
+
+	up_write(&data->sem);
 	return count;
 }
 
